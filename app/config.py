947fedde308669bdd108e894eba2ec22e0577e02
Dylan <dylmckaydev@gmail.com>
2026-08-17 22:48:51 -0400

    refactor: add postgres url parts to config

--- app/config.py
+++ app/config.py
@@ -6,6 +6,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: PostgresDsn
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
