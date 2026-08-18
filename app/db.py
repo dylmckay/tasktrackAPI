6e70130fd36d7ecd7be70899f686ac8ce21fd62c
Dylan <dylmckaydev@gmail.com>
2026-08-17 21:02:36 -0400

    feat: add get_db() dependency

--- app/db.py
+++ app/db.py
@@ -1,7 +1,18 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from .config import get_settings
 
 settings = get_settings()
 
 engine = create_async_engine(settings.database_url.unicode_string())
+
+async_session_maker = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession]:
+    async with async_session_maker() as session:
+        yield session
